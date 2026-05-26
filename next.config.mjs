cb8df3c1fe15d725d98ab652e620ae50c8a15aa5
Lakshya Kashyap <yashjha0241@gmail.com>
2026-05-26 17:38:04 +0530

    fiex issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-import nextPWA from "next-pwa";
+import nextPwa from "next-pwa";
 
-const withPWA = nextPWA({
+const withPWA = nextPwa({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 export default withPWA(nextConfig);
