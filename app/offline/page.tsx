a27f8097be1dfe63973868aac01f19456b041d30
Lakshya Kashyap <yashjha0241@gmail.com>
2026-05-26 17:49:12 +0530

    fix pwa issue

---
+++ app/offline/page.tsx
@@ -0,0 +1,7 @@
+export default function OfflinePage() {
+    return (
+        <div className="flex h-screen items-center justify-center text-2xl">
+            Offline Mode
+        </div>
+    );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
-import nextPwa from "next-pwa";
+import nextPWA from "next-pwa";
 
-const withPWA = nextPwa({
+const withPWA = nextPWA({
   dest: "public",
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  fallbacks: {
+    document: "/offline",
+  },
 });
 
-const nextConfig = {
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default withPWA(nextConfig);
